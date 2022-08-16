575aa50665af27782a94c6c1ce003f7df54fdeb3
anhdotdo <dongocanh15798@gmail.com>
2022-08-16 14:48:25 +0700

    INIT repo

---
+++ main.tf
@@ -0,0 +1,42 @@
+# Require TF version to be same as or greater than 0.12.13
+terraform {
+  required_version = ">=0.12.13"
+   backend "s3" {
+    bucket         = "s3-anhdo"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "aws-locks"
+    encrypt        = true
+   }
+}
+
+# Download any stable version in AWS provider of 2.36.0 or higher in 2.36 train
+provider "aws" {
+  region  = "us-east-1"
+  #version = "~> 2.36.0"
+}
+
+# Call the seed_module to build our ADO seed info
+module "bootstrap" {
+  source                      = "./modules/bootstrap"
+  name_of_s3_bucket           = "s3-anhdo"
+  dynamo_db_table_name        = "aws-locks"
+}
+
+# Build the VPC 
+
+resource "aws_vpc" "vpc" { 
+
+  cidr_block           = "10.1.0.0/16" 
+
+  instance_tenancy     = "default" 
+
+  tags = { 
+
+    Name      = "Vpc" 
+
+    Terraform = "true" 
+
+  } 
+
+} 
