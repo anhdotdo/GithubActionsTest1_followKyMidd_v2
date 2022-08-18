f8e24938a9f861d582d5f23c6c5e4835cb783928
anhdotdo <dongocanh15798@gmail.com>
2022-08-18 10:40:41 +0700

    ADD ecs cluster, service, task

--- main.tf
+++ main.tf
@@ -55,5 +55,47 @@ policy = <<EOF
 EOF
 }
 
+resource "aws_ecs_cluster" "demo-ecs-cluster" { 
+  name = "ecs-cluster-for-demo" 
+} 
+ 
+resource "aws_ecs_service" "demo-ecs-service-two" { 
+  name            = "demo-app" 
+  cluster         = aws_ecs_cluster.demo-ecs-cluster.id 
+  task_definition = aws_ecs_task_definition.demo-ecs-task-definition.arn 
+  launch_type     = "FARGATE" 
+  network_configuration { 
+    subnets          = ["subnet-00266a905d7a5c90c"] 
+    assign_public_ip = true 
+  } 
+  desired_count = 1 
+} 
+ 
+resource "aws_ecs_task_definition" "demo-ecs-task-definition" { 
+  family                   = "ecs-task-definition-demo" 
+  network_mode             = "awsvpc" 
+  requires_compatibilities = ["FARGATE"] 
+  memory                   = "1024" 
+  cpu                      = "512" 
+  execution_role_arn       = "arn:aws:iam::335856564507:role/aws-service-role/ecs.amazonaws.com/AWSServiceRoleForECS" 
+  container_definitions    = <<EOF
+  [
+    { 
+      "name": "demo-container", 
+      "image": "335856564507.dkr.ecr.us-east-1.amazonaws.com/demo-repo:5ce176634bce1a437376815d5d222b07bac867e9", 
+      "memory": 1024, 
+      "cpu": 512, 
+      "essential": true, 
+      "entryPoint": ["/"], 
+      "portMappings": [ 
+        { 
+          "containerPort": 6565, 
+          "hostPort": 5000
+        } 
+      ] 
+    } 
+  ]
+  EOF
+} 
 
 
