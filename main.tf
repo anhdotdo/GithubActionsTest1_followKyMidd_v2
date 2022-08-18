719cf0fceda5aaf943204f9ebb80b62829bd2759
anhdotdo <dongocanh15798@gmail.com>
2022-08-18 07:00:08 +0700

    CREATE ecr

--- main.tf
+++ main.tf
@@ -23,4 +23,36 @@ module "bootstrap" {
   dynamo_db_table_name        = "aws-locks"
 }
 
+# ECR 
+resource "aws_ecr_repository" "demo-repository" { 
+  name                 = "demo-repo" 
+  image_tag_mutability = "IMMUTABLE" 
+} 
+ 
+resource "aws_ecr_repository_policy" "demo-repo-policy" { 
+  repository = aws_ecr_repository.demo-repository.name 
+  policy     = <<EOF 
+  { 
+    "Version": "2008-10-17", 
+    "Statement": [ 
+      { 
+        "Sid": "adds full ecr access to the demo repository", 
+        "Effect": "Allow", 
+        "Principal": "*", 
+        "Action": [ 
+          "ecr:BatchCheckLayerAvailability", 
+          "ecr:BatchGetImage", 
+          "ecr:CompleteLayerUpload", 
+          "ecr:GetDownloadUrlForLayer", 
+          "ecr:GetLifecyclePolicy", 
+          "ecr:InitiateLayerUpload", 
+          "ecr:PutImage", 
+          "ecr:UploadLayerPart" 
+        ] 
+      } 
+    ] 
+  } 
+  EOF 
+} 
+
 
