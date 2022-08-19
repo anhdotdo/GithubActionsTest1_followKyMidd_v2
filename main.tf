7ad48e4154ec7750d3cd13bd3741aafa413783bd
anhdotdo <dongocanh15798@gmail.com>
2022-08-19 17:49:59 +0700

    MODIFY

--- main.tf
+++ main.tf
@@ -140,7 +140,8 @@ resource "aws_ecs_task_definition" "demo-ecs-task-definition" {
 
             "awslogs-region": "us-east-1", 
 
-            "awslogs-stream-prefix": "ecs" 
+            "awslogs-stream-prefix": "ecs", 
+            "awslogs-create-group": "true"
         }
 
       },
